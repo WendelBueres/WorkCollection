bda07a250736a579a53a2ec49ec86b6c8f45e2cb
Everton Vieira <vieiraeverton33@hotmail.com>
2022-10-31 19:47:41 -0300

    feat: adicionado rotas no app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import "reflect-metadata";
 import express from "express";
 import "express-async-errors";
-
+import userRoutes from "./Routes/user.router";
+import loginRoutes from "./Routes/login.router";
+import categoryRoutes from "./Routes/category.router";
+import projectRoutes from "./Routes/project.router"
 const app = express();
 
 app.use(express.json());
+app.use("/users", userRoutes)
+app.use("/login", loginRoutes)
+app.use("/projects", projectRoutes)
+app.use("/categories", categoryRoutes)
 
 export default app;
