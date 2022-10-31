c53992846e8c3d03e93cbf66c863de341701e715
Wendel Bueres <wendelbueres@gmail.com>
2022-10-31 17:49:42 -0300

    feat: implementado middleware isAdm

---
+++ src/middlewares/isAdm.midleware.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors";
+import jwt from "jsonwebtoken";
+
+async function isAdminMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  let token = req.params.authorization;
+
+  if (!token) {
+    throw new AppError("Missing authorization headers");
+  }
+
+  token = token.split(" ")[1];
+
+  jwt.verify(token, process.env.SECRET_KEY as string, (error, decoded: any) => {
+    if (error) {
+      throw new AppError("Unauthorized", 403);
+    }
+
+    req.user = {
+      id: decoded.sub,
+      isAdm: decoded.isAdm,
+    };
+
+    if (req.user.isAdm === false) {
+      throw new AppError("Unauthorized, you must have admin privileges", 403);
+    }
+
+    next();
+  });
+}
+
+export default isAdminMiddleware;
