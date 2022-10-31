519b6e18594caf2f00c67ec40f4f8f6564d248a7
Wendel Bueres <wendelbueres@gmail.com>
2022-10-31 17:21:09 -0300

    feat: implementada interface Contact

---
+++ src/interfaces/contact/index.ts
@@ -0,0 +1,10 @@
+export interface IContactRequest {
+  linkedin: string;
+  github: string;
+  phone: string;
+  userId: string;
+}
+
+export interface IContact extends IContactRequest {
+  id: string;
+}
