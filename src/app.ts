3a42e87c2db4c1642a7fbb3902f57a8e210e3aba
Wendel Bueres <wendelbueres@gmail.com>
2022-10-31 17:29:00 -0300

    feat: implementada chamada de handleError

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata";
 import express from "express";
 import "express-async-errors";
+import { handleErrorMidleware } from "./middlewares/handleError.midleware";
 
 const app = express();
 
 app.use(express.json());
+app.use(handleErrorMidleware);
 
 export default app;
