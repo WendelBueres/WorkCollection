c3ce27c29fedf3284a735c9d0ac2cd480a1ec8d7
Allan da Mota Salatino <allan.smota@gmail.com>
2022-11-01 13:45:25 -0300

    feat: listedProjectsService

---
+++ src/service/listProjects.service.ts
@@ -0,0 +1,13 @@
+import AppDataSource from "../data-source";
+import { Project } from "../entities/project.entity";
+
+const listProjectsService = async() => {
+
+    const projectRepository = AppDataSource.getRepository(Project)
+
+    const list = await projectRepository.find()
+
+    return list
+}
+
+export default listProjectsService;
