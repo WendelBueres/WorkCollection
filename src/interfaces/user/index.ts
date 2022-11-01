09055d0e6d8bc7ca5c5d96e982f37ab795a80ba0
Wendel Bueres <wendelbueres@gmail.com>
2022-11-01 12:31:03 -0300

    feat: adicionada nova interface

--- src/interfaces/user/index.ts
+++ src/interfaces/user/index.ts
@@ -2,10 +2,15 @@ export interface IUserRequest {
   name: string;
   email: string;
   password: string;
-  image: string;
+  image?: string;
   bio: string;
 }
 
 export interface IUser extends IUserRequest {
   id: string;
+}
+
+export interface IUserLogin {
+  email: string;
+  password: string;
 }
