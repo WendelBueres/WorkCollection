bcf6f600e145ac390836476f6c608610f1567488
Wendel <54292755+WendelBueres@users.noreply.github.com>
2022-11-01 11:10:33 -0300

    fix: apagadas declarações em duplicidade

--- src/app.ts
+++ src/app.ts
@@ -13,10 +13,6 @@ app.use("/users", userRoutes)
 app.use("/login", loginRoutes)
 app.use("/projects", projectRoutes)
 app.use("/categories", categoryRoutes)
-
-const app = express();
-
-app.use(express.json());
 app.use(handleErrorMidleware);
 
 export default app;
