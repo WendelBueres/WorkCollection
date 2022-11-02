da977a208f35e71e57a8815bb1e477d348aabcf1
eliseubrito <eliseubrito@protonmail.com>
2022-11-02 02:40:35 -0300

    feat: created controllers from delete and update project

---
+++ src/controllers/project/deleteProject.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import deleteProjectService from "../../services/deleteProject.services";
+
+const deleteProjectController = async (req: Request, res: Response) => {
+  const id: string = req.params.id;
+
+  const deleteProject = await deleteProjectService(id);
+
+  return res.status(202).json({ message: "Successfully Deleted" });
+};
+
+export default deleteProjectController;
