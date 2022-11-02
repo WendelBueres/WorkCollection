61c03daf927540780a5836fcb0287a98d990fc94
eliseubrito <eliseubrito@protonmail.com>
2022-11-02 02:41:32 -0300

    feat: created services from delete and update project

---
+++ src/services/deleteProject.services.ts
@@ -0,0 +1,25 @@
+import AppDataSource from "../data-source";
+import { Project } from "../entities/project.entity";
+import { AppError } from "../errors";
+// import { IProject } from "../interfaces/projects";
+
+const deleteProjectService = async (id: string) => {
+  const projectRepository = AppDataSource.getRepository(Project);
+
+  const searchProject = await projectRepository.findOneBy({ id });
+
+  if (!searchProject) {
+    throw new AppError("Project Not Found", 404);
+  } else {
+    const projectUpdate = await projectRepository
+      .createQueryBuilder()
+      .delete()
+      .from(Project)
+      .where("id = :id", { id: id })
+      .execute();
+    // console.log(projectUpdate)
+    return projectUpdate;
+  }
+};
+
+export default deleteProjectService;
---
+++ src/services/updateProject.services.ts
@@ -0,0 +1,30 @@
+import AppDataSource from "../data-source";
+import { Project } from "../entities/project.entity";
+import { AppError } from "../errors";
+import { IProjectRequest } from "../interfaces/projects";
+
+const updateProjectService = async (
+  id: string,
+  { name, category, image, link, technology, userId }: IProjectRequest
+) => {
+  const projectRepository = AppDataSource.getRepository(Project);
+
+  const searchProject = await projectRepository.findOneBy({ id });
+  if (!searchProject) {
+    throw new AppError("Project Not Found", 404);
+  } else {
+    projectRepository.update(id, {
+      name: name ? name : searchProject.name,
+      category: category ? category : searchProject.category,
+      image: image ? image : searchProject.image,
+      link: link ? link : searchProject.link,
+      technology: technology ? technology : searchProject.technology,
+    });
+
+    const projectUpdated = await projectRepository.findOneBy({ id });
+
+    return projectUpdated!;
+  }
+};
+
+export default updateProjectService;
