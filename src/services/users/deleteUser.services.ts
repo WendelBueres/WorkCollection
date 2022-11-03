8ee36343ffd6cfdfa509f7545ded26737c50989a
Everton Vieira <vieiraeverton33@hotmail.com>
2022-11-03 10:45:17 -0300

    fix: corrigido delete user

--- src/services/users/deleteUser.services.ts
+++ src/services/users/deleteUser.services.ts
@@ -2,21 +2,18 @@ import AppDataSource from "../../data-source";
 import { User } from "../../entities/user.entity";
 import { AppError } from "../../errors";
 
-const deleteUserService = async (id: string) => {
-  const userRepository = AppDataSource.getRepository(User);
+const deleteUserService = async(id: string) => {
+    const userRepository = AppDataSource.getRepository(User)
 
-  const findUser = await userRepository.findOneBy({ id });
+    const findUser = await userRepository.findOneBy({id})
 
-  if (!findUser) {
-    throw new AppError("User not found", 404);
-  }
+    if(!findUser){
+        throw new AppError("User not found", 404)
+    }
+    
+    await userRepository.delete(id)
 
-  const deleteUser = await userRepository
-    .createQueryBuilder()
-    .delete()
-    .from(User)
-    .where("id = :id", { id: id })
-    .execute();
+    return "User deleted with sucess!"
 };
 
 export default deleteUserService;
