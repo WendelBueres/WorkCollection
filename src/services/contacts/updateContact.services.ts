8888f5d645eaa8c097602bb0903ae3fe9018a0b7
Wendel Bueres <wendelbueres@gmail.com>
2022-11-03 18:32:59 -0300

    refactor: service updateContact

--- src/services/contacts/updateContact.services.ts
+++ src/services/contacts/updateContact.services.ts
@@ -3,18 +3,22 @@ import { Contact } from "../../entities/contact.entity";
 import { User } from "../../entities/user.entity";
 import { AppError } from "../../errors";
 
-const updateContactService = async (data: any, id: string) => {
-  const contactRepository = AppDataSource.getRepository(Contact);
-  const contactExists = await contactRepository.findOneBy({ id: id });
+const updateContactService = async (id: string, data: any) => {
+  console.log("AQUI!!!!", data);
+  const userRepository = AppDataSource.getRepository(User);
+  let user = await userRepository.findOneBy({ id: id });
 
-  if (!contactExists) {
-    throw new AppError("contact not found", 404);
+  if (!user) {
+    throw new AppError("User not found", 404);
   }
 
-  contactRepository.update(id, data);
-  const update = await contactRepository.findOneBy({ id: id });
-
-  return update;
+  console.log("DATA", data);
+  const contactRepository = AppDataSource.getRepository(Contact);
+  await contactRepository.update(user.contact.id, data);
+  user = await userRepository.findOneBy({ id: id });
+  if (user) {
+    return { message: "update success", contact: user.contact };
+  }
 };
 
 export default updateContactService;
