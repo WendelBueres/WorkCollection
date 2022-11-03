4397ea98a4bac105bd4099c3385cfbde80428c65
Wendel Bueres <wendelbueres@gmail.com>
2022-11-03 00:15:13 -0300

    feat: adicionado controller updateUser em rotas

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import createUserController from "../controllers/user/createUser.controller";
 import deleteUserController from "../controllers/user/deleteUser.controller";
+import updateUserController from "../controllers/user/updateUser.controller";
 
 const userRoutes = Router();
 
 userRoutes.post("", createUserController);
 userRoutes.get("");
-userRoutes.patch("/:id");
+userRoutes.patch("/:id", updateUserController);
 userRoutes.delete("/:id", deleteUserController);
 
 export default userRoutes;
