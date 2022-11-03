7b713602352350cd0408e549214803fb04026857
Wendel Bueres <wendelbueres@gmail.com>
2022-11-03 19:33:23 -0300

    fix: service updateContact

--- src/services/contacts/updateContact.services.ts
+++ src/services/contacts/updateContact.services.ts
@@ -1,9 +1,9 @@
 import AppDataSource from "../../data-source";
 import { Contact } from "../../entities/contact.entity";
+import { User } from "../../entities/user.entity";
 import { AppError } from "../../errors";
-import { IContactRequest } from "../../interfaces/contact";
 
-const updateContactService = async (id: string, data: any) => {
+const updateContactService = async (data: any, id: string) => {
   const userRepository = AppDataSource.getRepository(User);
   let user = await userRepository.findOneBy({ id: id });
 
@@ -11,11 +11,15 @@ const updateContactService = async (id: string, data: any) => {
     throw new AppError("User not found", 404);
   }
 
+  if (data.id) {
+    throw new AppError("id is read only");
+  }
+
   const contactRepository = AppDataSource.getRepository(Contact);
   await contactRepository.update(user.contact.id, data);
   user = await userRepository.findOneBy({ id: id });
   if (user) {
-    return { message: "update success", contact: user.contact };
+    return user.contact;
   }
 };
 
