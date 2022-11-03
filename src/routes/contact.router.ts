afa0c70ad6b77abc87f6f67adeb6b24ee4a208e1
Wendel Bueres <wendelbueres@gmail.com>
2022-11-03 18:33:24 -0300

    refactor: atualizadas rotas

--- src/routes/contact.router.ts
+++ src/routes/contact.router.ts
@@ -1,14 +1,13 @@
 import { Router } from "express";
-import deleteContactController from "../controllers/contacts/deleteContect.controller";
+import deleteContactController from "../controllers/contacts/deleteContact.controller";
 import listContactController from "../controllers/contacts/listContact.controller";
 import updateContactController from "../controllers/contacts/updateContact.controller";
-import createContactService from "../services/contacts/createContact.services";
 
 const contactRoutes = Router();
 
-contactRoutes.post("", createContactService);
 contactRoutes.get("", listContactController);
 contactRoutes.patch("/:id", updateContactController);
 contactRoutes.delete("/:id", deleteContactController);
+contactRoutes.delete("/:id/:contact", deleteContactController);
 
 export default contactRoutes;
