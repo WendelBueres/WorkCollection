9f926ad0f5e36018a6410956e4489c6e2f4adb89
eliseubrito <eliseubrito@protonmail.com>
2022-11-03 11:06:06 -0300

    fix: fixed delete contact controller and service

--- src/controllers/contacts/deleteContect.controller.ts
+++ src/controllers/contacts/deleteContect.controller.ts
@@ -4,9 +4,9 @@ import deleteContactService from "../../services/contacts/deleteContact.services
 const deleteContactController = async (req: Request, res: Response) => {
   const id = req.params.id;
 
-  await deleteContactService(id);
+  const deleteContact = await deleteContactService(id);
 
-  return res.status(204).json({ message: "User deleted" });
+  return res.status(204).json(deleteContact);
 };
 
 export default deleteContactController;
