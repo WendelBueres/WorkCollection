3cc36eaa4965d831c16ae23de8710e9d7fd6a732
Wendel Bueres <wendelbueres@gmail.com>
2022-11-04 18:25:42 -0300

    feat: verificacoes no services createProject

--- src/services/projects/createProject.services.ts
+++ src/services/projects/createProject.services.ts
@@ -1,31 +1,37 @@
 import AppDataSource from "../../data-source";
 import { Project } from "../../entities/project.entity";
 import { User } from "../../entities/user.entity";
-import { IProject } from "../../interfaces/projects";
+import { AppError } from "../../errors";
+import { IProject, IProjectRequest } from "../../interfaces/projects";
 
-const createProjectService = async ({
-  id,
-  name,
-  category,
-  image,
-  link,
-  technology,
-  userId,
-}: IProject): Promise<Project> => {
+const createProjectService = async (data: IProjectRequest) => {
   const projectRepository = AppDataSource.getRepository(Project);
   const userRepository = AppDataSource.getRepository(User);
+  const userExist = await userRepository.findOneBy({ id: data.userId });
 
-  const projectId = await userRepository.findOneBy({ id: userId });
+  if (!data.name) {
+    throw new AppError("name is a field required");
+  }
 
-  const project = projectRepository.create({
-    name: name,
-    category: category,
-    image: image,
-    link: link,
-    technology: technology,
-  });
+  if (!data.category) {
+    throw new AppError("category is a field required");
+  }
 
-  await projectRepository.save(project);
+  if (!data.userId) {
+    throw new AppError("userId is a field required");
+  }
+
+  if (!data.link) {
+    throw new AppError("link is a field required");
+  }
+
+  if (!userExist) {
+    throw new AppError("User not found", 404);
+  }
+
+  let project = projectRepository.create(data);
+
+  project = await projectRepository.save(project);
   return project;
 };
 
