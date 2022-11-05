6f9db2ade3747be0b1b86eebc5b1ade51cdf6757
Wendel Bueres <wendelbueres@gmail.com>
2022-11-04 22:15:50 -0300

    feat: atualizadas migrations

--- src/migrations/1667610592771-initial.ts
+++ src/migrations/1667610592771-initial.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class initial1667505252776 implements MigrationInterface {
+export class initial1667610592771 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
     }
