b1e27fc72a399ae850b670070ab4775b2b12b5fe
Wendel Bueres <wendelbueres@gmail.com>
2022-11-04 21:33:11 -0300

    refactor: efetuadas alteracoes para build

--- dist/src/data-source.js
+++ dist/src/data-source.js
@@ -11,14 +11,17 @@ const AppDataSource = new typeorm_1.DataSource(process.env.NODE_ENV === "test"
     }
     : {
         type: "postgres",
-        host: process.env.DB_HOST,
-        port: 5432,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB,
+        url: process.env.DATABASE_URL,
+        ssl: process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
         logging: true,
         synchronize: false,
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
+        entities: process.env.NODE_ENV === "production"
+            ? ["dist/src/entities/*.ts"]
+            : ["src/entities/*.ts"],
+        migrations: process.env.NODE_ENV === "production"
+            ? ["dist/src/migrations/*.ts"]
+            : ["src/migrations/*.ts"],
     });
 exports.default = AppDataSource;
