e5645fb90415265e56a47847b03724b4ca10040d
Wendel Bueres <wendelbueres@gmail.com>
2022-11-07 09:27:41 -0300

    fix: novas entities

--- src/migrations/1667716054458-initial.ts
+++ src/migrations/1667716054458-initial.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class initial1667505252776 implements MigrationInterface {
+export class initial1667716054458 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
     }
