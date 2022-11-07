9ebd10f3662fb9a453615af598d84da04b750b32
Wendel Bueres <wendelbueres@gmail.com>
2022-11-07 09:10:51 -0300

    feat: id do usuário agora é adquirido via token

--- src/controllers/project/updateProject.controller.ts
+++ src/controllers/project/updateProject.controller.ts
@@ -3,9 +3,10 @@ import updateProjectService from "../../services/projects/updateProject.services
 
 const updateProjectController = async (req: Request, res: Response) => {
   const id = req.params.id;
+  const userId = req.user.id;
   const projectInfo = req.body;
-  const update = await updateProjectService(id, projectInfo);
-  return res.status(204).json(update);
+  const update = await updateProjectService(id, userId, projectInfo);
+  return res.status(200).json(update);
 };
 
 export default updateProjectController;
