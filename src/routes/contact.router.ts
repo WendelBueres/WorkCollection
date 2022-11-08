3e62f0c5b6fc45ae1f643108abbf50cceb3ab4c0
Everton Vieira <vieiraeverton33@hotmail.com>
2022-11-08 15:00:31 -0300

    fix: adicionando auth token em contatos

--- src/routes/contact.router.ts
+++ src/routes/contact.router.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import deleteContactController from "../controllers/contacts/deleteContact.controller";
 import listContactController from "../controllers/contacts/listContact.controller";
 import updateContactController from "../controllers/contacts/updateContact.controller";
+import hasAuthMiddleware from "../middlewares/hasAuth.middleware";
 
 const contactRoutes = Router();
 
 contactRoutes.get("", listContactController);
-contactRoutes.patch("/:id", updateContactController);
-contactRoutes.delete("/:id", deleteContactController);
-contactRoutes.delete("/:id/:contact", deleteContactController);
+contactRoutes.patch("/:id", hasAuthMiddleware, updateContactController);
+contactRoutes.delete("/:id", hasAuthMiddleware, deleteContactController);
+contactRoutes.delete("/:id/:contact", hasAuthMiddleware, deleteContactController);
 
 export default contactRoutes;
