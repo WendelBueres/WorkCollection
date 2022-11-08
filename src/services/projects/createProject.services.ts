94411dabd0a2d5a0440208df0d0b99cc5d3521e4
Wendel Bueres <wendelbueres@gmail.com>
2022-11-08 18:12:22 -0300

    fix: corrigido service createProject

--- src/services/projects/createProject.services.ts
+++ src/services/projects/createProject.services.ts
@@ -1,13 +1,35 @@
 import AppDataSource from "../../data-source";
 import { Project } from "../../entities/project.entity";
+import { ProjectTech } from "../../entities/projectTechs.entity";
+import { Tech } from "../../entities/tech.entity";
 import { User } from "../../entities/user.entity";
 import { AppError } from "../../errors";
-import { IProject, IProjectRequest } from "../../interfaces/projects";
+import { IProjectRequest } from "../../interfaces/projects";
+import { IProjectTechsRequest } from "../../interfaces/ProjectTechs";
 
 const createProjectService = async (data: IProjectRequest, userId: string) => {
+  const projectTechsRepository = AppDataSource.getRepository(ProjectTech);
   const projectRepository = AppDataSource.getRepository(Project);
+  const techRepository = AppDataSource.getRepository(Tech);
   const userRepository = AppDataSource.getRepository(User);
   const userExist = await userRepository.findOneBy({ id: userId });
+
+  if (!data.techsId) {
+    throw new AppError(
+      `techsId is required, if you have not created a technology, proceed with the creation through the POST endpoint: /technologies`
+    );
+  }
+
+  await Promise.all(
+    data.techsId.map(async (element) => {
+      const { id } = element;
+      const checkTech = await techRepository.findOneBy({ id: id });
+      console.log(checkTech);
+      if (checkTech === null) {
+        throw new AppError(`techId: ${id} not find`, 404);
+      }
+    })
+  );
 
   if (!data.name) {
     throw new AppError("name is a field required");
@@ -21,21 +43,27 @@ const createProjectService = async (data: IProjectRequest, userId: string) => {
     throw new AppError("link is a field required");
   }
 
-  if (!data.technology) {
-    throw new AppError(
-      `technology is required, if you have not created a technology, proceed with the creation through the POST endpoint: /technologies`
-    );
-  }
-
   if (!userExist) {
     throw new AppError("User not found", 404);
   }
 
   let project = projectRepository.create(data);
   project.userId = userExist.id;
-
   project = await projectRepository.save(project);
-  return project;
+
+  let arrayTech: ProjectTech[] = [];
+
+  data.techsId.forEach(async (idTech) => {
+    const projectTechs: IProjectTechsRequest = {
+      projectsId: project.id,
+      techsId: idTech.id,
+    };
+    let projectTechCreated = projectTechsRepository.create(projectTechs);
+    projectTechCreated = await projectTechsRepository.save(projectTechCreated);
+    arrayTech.push(projectTechCreated);
+  });
+
+  return await projectRepository.findOneBy({ id: project.id });
 };
 
 export default createProjectService;
