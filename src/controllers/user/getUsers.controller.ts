6c472a0937972739e72e39ea6026d048a971366d
Everton Vieira <vieiraeverton33@hotmail.com>
2022-11-08 16:09:50 -0300

    feat: adicionado instance to plain no getUser

--- src/controllers/user/getUsers.controller.ts
+++ src/controllers/user/getUsers.controller.ts
@@ -1,10 +1,10 @@
+import { instanceToPlain } from "class-transformer";
 import { Request, Response } from "express";
 import getUsersServices from "../../services/users/getUsers.services";
 
 const getUsersController = async (req: Request, res: Response) => {
   const users = await getUsersServices();
-
-  return res.status(200).json(users);
+  return res.status(200).json(instanceToPlain(users));
 };
 
 export default getUsersController;
