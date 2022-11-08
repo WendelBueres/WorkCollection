13a9f60ecd47fa1e8e4ba35f3bc19775d29445af
Wendel Bueres <wendelbueres@gmail.com>
2022-11-08 18:12:03 -0300

    feat: adicionado endpoint listTechById

--- src/routes/technologies.router.ts
+++ src/routes/technologies.router.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import createTechController from "../controllers/techs/createTechController";
 import deleteTechController from "../controllers/techs/deleteTech.controller";
-import listTechsController from "../controllers/techs/listTechsController";
+import listTechsController from "../controllers/techs/listTechs.controller";
+import listTechsByIdController from "../controllers/techs/listTechsById.controller";
 import updateTechController from "../controllers/techs/updateTech.controller";
 import hasAuthMiddleware from "../middlewares/hasAuth.middleware";
 
@@ -10,6 +11,7 @@ const technologiesRouter = Router();
 technologiesRouter.post("", hasAuthMiddleware, createTechController);
 technologiesRouter.get("", listTechsController);
 technologiesRouter.patch("/:id", updateTechController);
+technologiesRouter.get("/:id", listTechsByIdController);
 technologiesRouter.delete("/:id", deleteTechController);
 
 export default technologiesRouter;
