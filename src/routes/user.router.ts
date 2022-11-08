aa309f7cd9e619368709f4d5fdaf7eb7cb6392a3
Everton Vieira <vieiraeverton33@hotmail.com>
2022-11-08 14:59:52 -0300

    fix: adicionando auth token em usuario

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -4,13 +4,14 @@ import deleteUserController from "../controllers/user/deleteUser.controller";
 import getUsersController from "../controllers/user/getUsers.controller";
 import getUsersByIdController from "../controllers/user/getUsersById.controller.";
 import updateUserController from "../controllers/user/updateUser.controller";
+import hasAuthMiddleware from "../middlewares/hasAuth.middleware";
 
 const userRoutes = Router();
 
 userRoutes.post("", createUserController);
 userRoutes.get("", getUsersController);
 userRoutes.get("/:id", getUsersByIdController);
-userRoutes.patch("/:id", updateUserController);
-userRoutes.delete("/:id", deleteUserController);
+userRoutes.patch("/:id", hasAuthMiddleware, updateUserController);
+userRoutes.delete("/:id", hasAuthMiddleware, deleteUserController);
 
 export default userRoutes;
