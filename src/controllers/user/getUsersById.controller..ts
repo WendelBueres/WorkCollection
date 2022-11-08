86b047a45ca96bd50eebfa9c4a9302b745f38971
Everton Vieira <vieiraeverton33@hotmail.com>
2022-11-08 16:09:54 -0300

    feat: adicionado instance to plain no getUserId

--- src/controllers/user/getUsersById.controller..ts
+++ src/controllers/user/getUsersById.controller..ts
@@ -1,11 +1,11 @@
+import { instanceToPlain } from "class-transformer";
 import { Request, Response } from "express";
 import getUsersByIdServices from "../../services/users/getUsersById.services";
 
 const getUsersByIdController = async (req: Request, res: Response) => {
   const { id } = req.params;
   const user = await getUsersByIdServices(id);
-
-  return res.status(200).json(user);
+  return res.status(200).json(instanceToPlain(user));
 };
 
 export default getUsersByIdController;
