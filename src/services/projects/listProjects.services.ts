d10874ea04df07235d8b92e545ae2c2bdad902ce
Wendel Bueres <wendelbueres@gmail.com>
2022-11-08 18:12:49 -0300

    feat: adicionada relations

--- src/services/projects/listProjects.services.ts
+++ src/services/projects/listProjects.services.ts
@@ -4,7 +4,7 @@ import { Project } from "../../entities/project.entity";
 const listProjectsService = async () => {
   const projectRepository = AppDataSource.getRepository(Project);
 
-  const list = await projectRepository.find();
+  const list = await projectRepository.find({ relations: { techs: true } });
 
   return list;
 };
