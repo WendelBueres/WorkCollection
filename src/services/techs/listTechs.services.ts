ec089b91578d66502d6462e48d0564825ed14954
Wendel Bueres <wendelbueres@gmail.com>
2022-11-09 10:53:00 -0300

    refactor: refatorada resposta

--- src/services/techs/listTechs.services.ts
+++ src/services/techs/listTechs.services.ts
@@ -4,7 +4,7 @@ import { Tech } from "../../entities/tech.entity";
 const listTechsService = async () => {
   const techsRepository = AppDataSource.getRepository(Tech);
 
-  const list = await techsRepository.find({ relations: { user: true } });
+  const list = await techsRepository.find();
 
   return list;
 };
