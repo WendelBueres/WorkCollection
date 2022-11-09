82e13bd7c4282861d8d6bb6d79f6bf0a0700016f
Wendel Bueres <wendelbueres@gmail.com>
2022-11-09 10:52:28 -0300

    feat: adicionada verificação de usuário

--- src/controllers/techs/updateTech.controller.ts
+++ src/controllers/techs/updateTech.controller.ts
@@ -4,7 +4,8 @@ import updateTechService from "../../services/techs/updateTech.services";
 const updateTechController = async (req: Request, res: Response) => {
   const id = req.params.id;
   const data = req.body;
-  const updated = await updateTechService(id, data);
+  const { id: userId } = req.user;
+  const updated = await updateTechService(id, userId, data);
   return res.status(200).json(updated);
 };
 
--- src/services/techs/updateTech.services.ts
+++ src/services/techs/updateTech.services.ts
@@ -3,16 +3,20 @@ import { Tech } from "../../entities/tech.entity";
 import { AppError } from "../../errors";
 import { ITechsRequest } from "../../interfaces/techs";
 
-const updateTechService = async (id: string, data: any) => {
+const updateTechService = async (id: string, userId: string, data: any) => {
   const TechRepository = AppDataSource.getRepository(Tech);
 
   const searchTech = await TechRepository.findOneBy({ id: id });
   if (!searchTech) {
     throw new AppError("technology not found", 404);
   }
-  
+
+  if (searchTech.userId !== userId) {
+    throw new AppError("user does not have permission", 403);
+  }
+
   if (data.id) {
-    throw new AppError("id is read only")
+    throw new AppError("id is read only");
   }
 
   await TechRepository.update(id, data);
